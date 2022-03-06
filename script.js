b1ffa0c988903b397a5ba4ca9c98b04de71d5dd4
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2022-03-06 17:23:58 -0300

    updating code

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.onscroll = function() {scrollPage()};
 
-function scrollPage() {
+
+function scrollPage() {    
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
     
-    document.querySelector('#myBar').style.width = scrolled + '%';
+    console.log(scrolled)
+    document.querySelector('#bar').style.width = scrolled + '%';
 }
